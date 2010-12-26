a11a679fe846d096d45e08278ae15f43c8d4dfe2
abhirama <none@none>
2010-12-26 18:03:05 +0530

    Moved the whole code into one huge monolithic class to avoid the method call stack over flow error.

--- src/com/abhirama/xmlsplitter/XmlSplitter.java
+++ src/com/abhirama/xmlsplitter/XmlSplitter.java
@@ -9,25 +9,29 @@ package com.abhirama.xmlsplitter;
 
 import org.codehaus.staxmate.SMOutputFactory;
 import org.codehaus.staxmate.out.SMOutputDocument;
+import org.codehaus.staxmate.out.SMOutputElement;
 
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.StartElement;
-import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.events.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.Stack;
 
-public class XmlSplitter implements XMLEventObserver {
+public class XmlSplitter {
   protected File xmlFile;
   protected XMLEventReader xmlEventReader;
   protected StartElement documentRootElement;
-  protected XMLDocumentWriter xmlDocumentWriter;
   protected long expectedFileSize; //in bytes
   protected int fileCounter = 0;
   protected File splitFileDirectory;
+  protected Stack<SMOutputElement> outputElements = new Stack<SMOutputElement>();
+  protected SMOutputDocument outputDocument;
+  protected File currentXMLOPFile;
 
   protected int nodePositionCounter = 0;
 
@@ -59,107 +63,112 @@ public class XmlSplitter implements XMLEventObserver {
     xmlEventReader = XMLInputFactory.newInstance().createXMLEventReader(new FileInputStream(this.xmlFile));
   }
 
-  public SMOutputDocument createNewOutputDocument(File outputFile) throws XMLStreamException {
-    SMOutputFactory outf = new SMOutputFactory(XMLOutputFactory.newInstance());
-    SMOutputDocument doc = outf.createOutputDocument(outputFile);
-
-    // (optional) 3: enable indentation (note spaces after backslash!)
-    doc.setIndentation("\n  ", 1, 1);
-    return doc;
-  }
-
   public void split() throws XMLStreamException {
     assignDocumentRootElement();
-    splitHelper();
+    assignCurrentXMLOPFile();
+    assignCurrentOPDocument(currentXMLOPFile);
+    opDocumentRootElement();
+    processEvents();
   }
 
-  static int bar = 0;
-  protected void splitHelper() throws XMLStreamException {
-    loopUntilStartElement();
-    System.out.println("Creating doc writer");
-    initializeXmlDocumentWriter();
-    System.out.println("Done creating doc writer");
-    fileCounter++;
-    System.out.println("Started to write");
-    System.out.println("--------" + ++bar);
-    xmlDocumentWriter.write();
+  protected void opDocumentRootElement() throws XMLStreamException {
+    SMOutputElement outputRootElement = opElement(documentRootElement, outputDocument);
+    outputElements.push(outputRootElement);
   }
 
-  protected void initializeXmlDocumentWriter() throws XMLStreamException {
-    xmlDocumentWriter = new XMLDocumentWriter(getSplitFile());
-    System.out.println("Initializing doc writer");
-    xmlDocumentWriter.init();
-    System.out.println("Done initializing doc writer");
-    xmlDocumentWriter.setDocumentRootElement(this.documentRootElement);
-    xmlDocumentWriter.setXmlEventReader(this.xmlEventReader);
-    xmlDocumentWriter.setXmlEventObserver(this);
-  }
-
-  protected File getSplitFile() {
-    return new File(splitFileDirectory, getSplitFileName());
-  }
-
-  protected String getSplitFileName() {
-    String locXmlFileName = this.xmlFile.getName();
-    String splitFileName = locXmlFileName.substring(0, locXmlFileName.lastIndexOf(".")) + "_" + fileCounter + ".xml";
-    System.out.println(splitFileName);
-    return splitFileName;
-  }
-
-  protected void loopUntilStartElement() throws XMLStreamException {
-    XMLEvent xmlEvent = xmlEventReader.peek();
-
-    System.out.println("<>");
-    while (!xmlEvent.isStartElement()) {
-      System.out.println("looping");
-      if (xmlEvent.isEndDocument() || !xmlEventReader.hasNext()) {
-        System.out.println("End reached - 1");
-        System.out.println("Document ends:" + xmlEvent.isEndDocument());
-        System.exit(0);
+  protected void processEvents() throws XMLStreamException {
+    while (xmlEventReader.hasNext()) {
+      XMLEvent xmlEvent = xmlEventReader.nextEvent();
+      if (xmlEvent.isStartElement()) {
+        StartElement startElement = xmlEvent.asStartElement();
+        SMOutputElement outputRootElement = opElement(startElement, outputElements.peek());
+        outputElements.push(outputRootElement);
+        nodePositionCounter++;
       }
 
-      this.xmlEventReader.nextEvent();
-      xmlEvent = xmlEventReader.peek();
-    }
-    System.out.println("</>");
-  }
+      if (xmlEvent.isCharacters()) {
+        Characters characters = xmlEvent.asCharacters();
+        opCharacters(characters, outputElements.peek());
+      }
 
-  public void notifyElementStartEvent(XMLEvent xmlEvent) {
-    nodePositionCounter++;
-  }
+      if (xmlEvent.isEndElement()) {
+        outputElements.pop();
+        if (outputElements.size() == 1) { //this is the root element that we added in the begining
+          flushOutput(); //doing this so that we can get the file size
 
-  int foo = 0;
-
-  public void notifyElementEndEvent(XMLEvent xmlEvent) {
-   if (foo == 251818) {
-     int k = 0;  
-   }
-
-    //System.out.println(++foo);
-
-    nodePositionCounter--;
-    try {
-      if (nodePositionCounter == 0) {
-        xmlDocumentWriter.flushOutput();
-
-        if (xmlDocumentWriter.getXmlFile().length() > this.expectedFileSize) {
-          xmlDocumentWriter.close();
-          System.out.println(xmlEvent.asEndElement().getName().getLocalPart()); //todo has to be removed
-          splitHelper();
+          if (currentXMLOPFile.length() > this.expectedFileSize) {
+            closeOpDocument();
+            outputElements.pop(); //we make this emty bcos we will be adding a new root element in the next step
+            assignCurrentXMLOPFile();
+            assignCurrentOPDocument(currentXMLOPFile);
+            opDocumentRootElement();
+          }
         }
       }
-    } catch (XMLStreamException e) {
-      throw new RuntimeException(e);
+
+      if (xmlEvent.isEndDocument()) {
+        closeOpDocument();
+      }
     }
   }
 
-  public void notifyDocumentEndEvent(XMLEvent xmlEvent) {
-    System.out.println("End reached - 0");
-    try {
-      xmlDocumentWriter.close();
-    } catch (XMLStreamException e) {
-      throw new RuntimeException(e);
+  public void closeOpDocument() throws XMLStreamException {
+    this.outputDocument.closeRootAndWriter();
+  }
+
+  protected SMOutputElement opElement(StartElement startElement, SMOutputDocument outputDocument) throws XMLStreamException {
+    SMOutputElement outputElement = opElementTag(startElement, outputDocument);
+    opNameSpaces(startElement, outputElement);
+    opAttributes(startElement, outputElement);
+    return outputElement;
+  }
+
+  protected SMOutputElement opElement(StartElement startElement, SMOutputElement outputElement) throws XMLStreamException {
+    SMOutputElement innerOutputElement = opElementTag(startElement, outputElement);
+    opNameSpaces(startElement, innerOutputElement);
+    opAttributes(startElement, innerOutputElement);
+    return innerOutputElement;
+  }
+
+  protected void opCharacters(Characters characters, SMOutputElement outputElement) throws XMLStreamException {
+    outputElement.addCharacters(characters.getData());
+  }
+
+  protected SMOutputElement opElementTag(StartElement startElement, SMOutputDocument outputDocument) throws XMLStreamException {
+    String elementName = Util.getNameFromQName(startElement.getName());
+    SMOutputElement outputElement = outputDocument.addElement(elementName);
+    return outputElement;
+  }
+
+  protected SMOutputElement opElementTag(StartElement startElement, SMOutputElement outputElement) throws XMLStreamException {
+    String elementName = Util.getNameFromQName(startElement.getName());
+    SMOutputElement innerOutputElement = outputElement.addElement(elementName);
+    return innerOutputElement;
+  }
+
+  protected void opNameSpaces(StartElement startElement, SMOutputElement outputElement) throws XMLStreamException {
+    Iterator nameSpaceIterator = startElement.getNamespaces();
+
+    while (nameSpaceIterator.hasNext()) {
+      Namespace namespace = ((Namespace) nameSpaceIterator.next());
+      org.codehaus.staxmate.out.SMNamespace smNamespace = outputElement.getNamespace(namespace.getNamespaceURI(), namespace.getPrefix());
+      outputElement.predeclareNamespace(smNamespace);
     }
+  }
+
+  protected void opAttributes(StartElement startElement, SMOutputElement outputElement) throws XMLStreamException {
+    Iterator attributeIterator = startElement.getAttributes();
+
+    while (attributeIterator.hasNext()) {
+      Attribute attribute = ((Attribute) attributeIterator.next());
+      String attributeName = Util.getNameFromQName(attribute.getName());
+      String attributeValue = attribute.getValue();
+      outputElement.addAttribute(attributeName, attributeValue);
+    }
+  }
+
+  protected void flushOutput() throws XMLStreamException {
+    this.outputDocument.getContext().flushWriter();
   }
 
   protected void assignDocumentRootElement() throws XMLStreamException {
@@ -170,6 +179,24 @@ public class XmlSplitter implements XMLEventObserver {
         break;
       }
     }
+  }
+
+  public void assignCurrentOPDocument(File outputFile) throws XMLStreamException {
+    SMOutputFactory smOutputFactory = new SMOutputFactory(XMLOutputFactory.newInstance());
+    outputDocument = smOutputFactory.createOutputDocument(outputFile);
+    // (optional) 3: enable indentation (note spaces after backslash!)
+    outputDocument.setIndentation("\n  ", 1, 1);
+  }
+
+  protected void assignCurrentXMLOPFile() {
+    currentXMLOPFile = new File(splitFileDirectory, getSplitFileName());
+  }
+
+  protected String getSplitFileName() {
+    String locXmlFileName = this.xmlFile.getName();
+    fileCounter = fileCounter + 1;
+    String splitFileName = locXmlFileName.substring(0, locXmlFileName.lastIndexOf(".")) + "_" + fileCounter + ".xml";
+    return splitFileName;
   }
 
   public static void main(String[] args) throws XMLStreamException, FileNotFoundException {
